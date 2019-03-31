ab75467097e270678c955968d4807243845118e4
ejrach <eric.rach@gmail.com>
2019-03-31 15:37:28 -0700

    Separated out the Volcanoes and Population into two layers which can be toggled on/off

--- Projects/Mapping/webmappingviafile.py
+++ Projects/Mapping/webmappingviafile.py
@@ -24,24 +24,32 @@ def color_producer(elevation):
 # Set the zoom on a certain area. Coordinates can eb obtained by going to google maps and right clicking "What's here?"
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
-# Set a feature group
-fg = folium.FeatureGroup(name="My Map")
+# Set a feature group for volcanoes
+fgv = folium.FeatureGroup(name="Volcanoes")
 
+### Add the Marker Layer ###
 # The zip method allows you to iterate through two lists, side by side
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m", 
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m", 
     fill_color=color_producer(el), color = 'grey', fill=True, fill_opacity=0.7))
+
+
+### Add the JSON Population Layer ###
+# Set a feature group for Population
+fgp = folium.FeatureGroup(name="Population")
 
 # Creates a GeoJson oject, add adds polygons. This json file could also contain lines as well
 data_json = open("world.json", 'r', encoding='utf-8-sig').read()
 
 # To understand how the population is obtained, ['properties']['POP2005'] is used 
 # to access the data.
-fg.add_child(folium.GeoJson(data=data_json, 
+fgp.add_child(folium.GeoJson(data=data_json, 
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map2.html")
 
